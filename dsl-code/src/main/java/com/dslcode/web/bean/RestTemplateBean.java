d9baae899abd1c46019b50715dd7213920fc98ad
dongsilin <dongsilin@51ishare.com>
2017-03-10 11:13:04 +0800

    创建RestTemplate session bean

--- dsl-code/src/main/java/com/dslcode/web/bean/RestTemplateBean.java
+++ dsl-code/src/main/java/com/dslcode/web/bean/RestTemplateBean.java
@@ -1,22 +1,28 @@
 package com.dslcode.web.bean;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.Serializable;
 import java.nio.charset.Charset;
 
 /**
  * Created by dongsilin on 2017/3/7.
  * RestTemplate bean，生命周期为session
  */
-@Configuration
-public class RestTemplateBean {
+@Component
+@Scope(value = "session",  proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Slf4j
+public class RestTemplateBean extends RestTemplate implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private static final SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
     private static final HttpMessageConverter httpMessageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
@@ -25,12 +31,15 @@ public class RestTemplateBean {
         requestFactory.setConnectTimeout(1000 * 30);    //30s
     }
 
-    @Bean @Scope(value = "session",  proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public RestTemplate initRestTemplate(){
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setRequestFactory(requestFactory);
-        restTemplate.getMessageConverters().set(1, httpMessageConverter);
-        return restTemplate;
+    @PostConstruct
+    public void init(){
+        this.setRequestFactory(requestFactory);
+        this.getMessageConverters().set(1, httpMessageConverter);
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        log.debug("**********************************RestTemplateBean PreDestroy******************************");
     }
 
 }
