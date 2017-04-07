8cc3175f8c3344a9acbd0977f1e1b7ee1a5b54e0
dongsilin <dongsilin@51ishare.com>
2017-04-07 09:42:46 +0800

    创建订单高并发demo 工程

---
+++ demo-spring-boot/demo-mall-order-high-concurrency/src/main/java/com/dslcode/demo/order/DemoMallOrderHighConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.dslcode.demo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMallOrderHighConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMallOrderHighConcurrencyApplication.class, args);
+	}
+}
